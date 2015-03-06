b9dbcc75ce03a1da62f1c6d9b918d82baa840d31
James Michael DuPont <JamesMikeDuPont@gmail.com>
2015-03-05 21:03:59 -0600

    update

--- src/make.bash
+++ src/make.bash
@@ -118,7 +118,7 @@ if [ ! -x "$GOROOT_BOOTSTRAP/bin/go" ]; then
 	echo "Set \$GOROOT_BOOTSTRAP to a working Go tree >= Go 1.4." >&2
 fi
 #rm -f cmd/dist/dist
-#GOROOT="$GOROOT_BOOTSTRAP" GOOS="" GOARCH="" "$GOROOT_BOOTSTRAP/bin/go" build -x  -work -v -o cmd/dist/dist -compiler gccgo ./cmd/dist
+GOROOT="$GOROOT_BOOTSTRAP" GOOS="" GOARCH="" "$GOROOT_BOOTSTRAP/bin/go" build -x  -work -v -o cmd/dist/dist -compiler gccgo ./cmd/dist
 
 # -e doesn't propagate out of eval, so check success by hand.
 eval $(./cmd/dist/dist env -p || echo FAIL=true)
